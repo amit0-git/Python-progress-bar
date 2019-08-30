e9392e199ba29beeb77fa43ad9fe6a722a4a594f
amit0-git <54703731+amit0-git@users.noreply.github.com>
2019-08-30 14:57:46 +0530

    Add files via upload

---
+++ loader.py
@@ -0,0 +1,25 @@
+#Author:Amit Verma
+
+import os
+import time
+
+def load():
+	tm=4
+	while tm>0:
+		print('Loading.')
+		time.sleep(0.1)
+		os.system('clear')
+		print('Loading...')
+		time.sleep(0.1)
+		os.system('clear')
+		print('Loading....')
+		time.sleep(0.1)
+		os.system('clear')
+		print('Loading......')
+		time.sleep(0.1)
+		os.system('clear')
+		print('Loading.......')
+		time.sleep(0.1)
+		os.system('clear')
+		tm-=1
+load()
