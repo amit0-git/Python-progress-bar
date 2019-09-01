a7556c0e86ae34fe0ac9135c10134c9299a7d2eb
Amit Verma <54703731+amit0-git@users.noreply.github.com>
2019-09-01 19:27:32 +0530

    Add files via upload

---
+++ loader2.py
@@ -0,0 +1,19 @@
+#Author:Amit Verma
+import time
+import sys
+
+done = 'end'
+def animate():
+    while done == 'end':
+        sys.stdout.write('\rLoading |')
+        time.sleep(0.1)
+        sys.stdout.write('\rLoading /')
+        time.sleep(0.1)
+        sys.stdout.write('\rLoading -')
+        time.sleep(0.1)
+        sys.stdout.write('\rLoading \\')
+        time.sleep(0.1)
+    
+
+animate()
+#long process here
